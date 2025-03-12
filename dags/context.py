131826b349b36f5e42fd01bad91c39e34810a978
mjain06 <92713031+mjain06@users.noreply.github.com>
2025-03-12 13:25:43 +0000

    test

--- dags/context.py
+++ dags/context.py
@@ -1,23 +1,34 @@
- from datetime import datetime
-
+from datetime import datetime
+import pprint
 from airflow.models import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-import pprint
- 
- print_context = PythonOperator(
+
+# Define the function to print the execution context
+def print_execution_context():
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint()
+
+# Define the DAG and its tasks
+with DAG(
+    dag_id="context_dag",
+    start_date=datetime(year=2019, month=1, day=1),
+    end_date=datetime(year=2019, month=1, day=5),
+    schedule="@daily",
+) as dag:
+
+    # Define the tasks
+    print_context = PythonOperator(
         task_id="print_execution_context",
         python_callable=print_execution_context,
         provide_context=True,  
     )
 
-    print_context
-    
- 
     bash_task = BashOperator(
         task_id="echo_task_message",
         bash_command="echo '[{{ task_instance.task_id }}] is running in the DAG pipeline'",
     )
 
+    # Set task dependencies
     print_context >> bash_task
